dd2f371d1dd176bd0171a825c5c568bc3819c1c0
dhlee94 <77007726+dhlee94@users.noreply.github.com>
2023-04-21 01:48:38 +0900

    Add files via upload

--- main_multiGPU.py
+++ main_multiGPU.py
@@ -1,14 +1,11 @@
-from utils.utils import AddParserManager, seed_everything, select_idx_for_train
+from utils.utils import AddParserManager, seed_everything
 import argparse
-from core.function import train_multi, valid_multi
-from core.criterion import MultiClassDiceCELoss
-import pickle
-import numpy as np
 from core.optimizer import CosineAnnealingWarmUpRestarts
 from data.dataset import ImageDataset_Classification
 from timm.models.layers import to_2tuple
 from torch.utils.data import DataLoader
-from models.EfficientNet import EfficientNet
+from models.ConvNext import ConvNeXt
+from models.CoatNet import CoatNet
 import torch
 import torch.nn as nn
 import os
@@ -24,7 +21,9 @@ from torch.utils.data.distributed import DistributedSampler
 from torch.nn.parallel import DistributedDataParallel
 import torch.distributed as dist
 import torch.multiprocessing as mp
-from utils.utils import save_checkpoint, save_checkpoint_remove_module
+from utils.utils import save_checkpoint, AverageMeter, classification_accruracy_multi
+from tqdm import tqdm
+from torch.autograd import Variable
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--seed', type=int,
@@ -134,7 +133,8 @@ def main_worker(gpu, ngpus_per_node, args):
             ]
         )
         
-    model = EfficientNet(in_channels=args.input_channel, out_channels=args.num_class)
+    model = ConvNeXt(in_chans=args.input_channel, num_classes=args.num_class)
+    #model = CoatNet(in_channels=args.input_channel, out_class=args.num_class, img_size=args.img_size)
     if not torch.cuda.is_available() and not torch.backends.mps.is_available():
         print('using CPU, this will be slow')
     elif args.distributed:
@@ -218,9 +218,9 @@ def main_worker(gpu, ngpus_per_node, args):
         file = open(os.path.join(log_path, f'{epoch}_log.txt'), 'a')
         if args.distributed:
             train_sampler.set_epoch(epoch)    
-        train_multi(model=model, write_iter_num=args.write_iter_num, train_dataset=trainloader, optimizer=optimizer, 
+        train(model=model, write_iter_num=args.write_iter_num, train_dataset=trainloader, optimizer=optimizer, 
                     device=device, criterion=criterion, epoch=epoch, file=file)
-        accuracy = valid_multi(model=model, write_iter_num=args.write_iter_num, valid_dataset=validloader, criterion=criterion, 
+        accuracy = valid(model=model, write_iter_num=args.write_iter_num, valid_dataset=validloader, criterion=criterion, 
                                device=device, epoch=epoch, file=file)
         scheduler.step()
         is_best = accuracy > best_loss
@@ -235,6 +235,57 @@ def main_worker(gpu, ngpus_per_node, args):
                 'scheduler' : scheduler.state_dict()
             }, is_best=is_best, path=args.model_save_path)
         file.close()
+
+def train(model=None, write_iter_num=5, train_dataset=None, optimizer=None, device=None, criterion=torch.nn.BCELoss(), epoch=None, file=None):
+    #scaler = torch.cuda.amp.GradScaler()
+    assert train_dataset is not None, print("train_dataset is none")
+    model.train()        
+    ave_accuracy = AverageMeter()
+    #scaler = torch.cuda.amp.GradScaler()
+    for idx, (Image, Label) in enumerate(tqdm(train_dataset)):
+        #model input data
+        Input = Variable(Image.to(device), requires_grad=False)
+        label = Variable(Label.to(device), requires_grad=False)
+        Output = model(Input)
+        loss = criterion(Output, label.squeeze(dim=-1))            
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        accuracy = classification_accruracy_multi(Output, label)
+        ave_accuracy.update(accuracy)
+        if idx % write_iter_num == 0:
+            tqdm.write(f'Epoch : {epoch} Iter : {idx}/{len(train_dataset)} '
+                       f'Loss : {loss :.4f} '
+                       f'Accuracy : {accuracy :.2f} ')
+        if idx % (2*write_iter_num) == 0:
+            tqdm.write(f'Epoch : {epoch} Iter : {idx}/{len(train_dataset)} '
+                    f'Loss : {loss :.4f} '
+                    f'Accuracy : {accuracy :.2f} ', file=file)
+    tqdm.write(f'Average Accuracy : {ave_accuracy.average() :.4f} \n\n')
+    tqdm.write(f'Average Accuracy : {ave_accuracy.average() :.4f} \n\n', file=file)
+    
+def valid(model=None, write_iter_num=5, valid_dataset=None, criterion=torch.nn.BCELoss(), device=None, epoch=None, file=None):
+    ave_accuracy = AverageMeter()
+    model.eval()
+    with torch.no_grad():
+        for idx, (Image, Label) in enumerate(tqdm(valid_dataset)):
+            #model input data
+            Input = Variable(Image.to(device), requires_grad=False)
+            label = Variable(Label.to(device), requires_grad=False)
+            Output = model(Input)
+            loss = criterion(Output, label.squeeze(dim=-1))
+            accuracy = classification_accruracy_multi(Output, label)
+            ave_accuracy.update(accuracy)
+            if idx % (write_iter_num) == 0:
+                tqdm.write(f'Epoch : {epoch} Iter : {idx}/{len(valid_dataset)} '
+                        f'Validation Loss : {loss :.2f} '
+                        f'Validation Accuracy : {accuracy :.2f} ')
+            if idx % (2*write_iter_num) == 0:
+                tqdm.write(f'Epoch : {epoch} Iter : {idx}/{len(valid_dataset)} '
+                            f'Validation Loss : {loss :.2f} '
+                            f'Validation Accuracy : {accuracy :.2f} ', file=file)
+        tqdm.write(f'Average Accuracy : {ave_accuracy.average() :.2f} \n', file=file)
+    return ave_accuracy.average()  
 
 if __name__ == '__main__':
     main()
